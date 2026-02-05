88749f2dbed49dbdf20e65cb653d94db46b0ab09
Irfan khan <ikan13545@gmail.com>
2026-02-05 09:07:05 +0530

    datatypes

---
+++ 01basics/02variables.js
@@ -0,0 +1,25 @@
+// alert(3+3);  we are using nodejs, not browser 
+
+
+// console.log(3+3); // this is how we print something in nodejs
+
+// variables in js
+// 3 ways to declare a variable in js
+// var, let, const
+// console.log("Irfan khan the founder")
+
+let name = "Irfan";
+ let age = 18;
+ let isLoggedIn = false;
+let state;
+
+ // number => 2 se lekr power of 53
+ // bigint jyada numbers ko store kregi bhot km use krte ha 
+ // string => " ", ' ', ` `
+// boolean => true, false
+// null => standard value;
+// undefined => value is not assigned..
+// symbol => unique value find krne k liye use krte ha;
+
+ console.log(typeof  null); //lekin iska object aata h kuch khte lanuage error ha 
+ console.log(typeof  undefined); // undefined hi aayega 
