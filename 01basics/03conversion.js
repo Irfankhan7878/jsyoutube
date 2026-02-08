5dfd4c4ce22bb92a590d9ded30b1beab3fd9f769
Irfan khan <ikan13545@gmail.com>
2026-02-08 15:55:41 +0530

    Added type conversion example

---
+++ 01basics/03conversion.js
@@ -0,0 +1,37 @@
+// let score = "33abc";
+
+// // console.log(typeof score)
+// // console.log(typeof(score));
+
+//  let valueInNumber = Number(score)
+// //  console.log(typeof valueInNumber);
+// //  console.log(valueInNumber);
+
+
+// //"33" => 33
+// // "33abc" => NAN
+// // true => false => 0
+// let isLoggedIn = "irfan"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// // 1 => true; or 0=> se false
+// // ""=> false
+// //"irfan"=> true
+// let stringNumber= String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let score = "33abc";
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);        // NaN
+console.log(typeof valueInNumber); // number
+
+let isLoggedIn = "irfan";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);    // true
+
+let someNumber = 10;
+let stringNumber = String(someNumber);
+console.log(stringNumber);         // "10"
+console.log(typeof stringNumber);  // string
